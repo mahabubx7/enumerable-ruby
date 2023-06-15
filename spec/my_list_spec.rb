99f99c9d07957a30957b3c7eb59f22457b4dc47e
Mahabub Alam <mahabubx7@gmail.com>
2023-06-15 21:21:00 +0600

    :hammer: Pair(Mahabub): fix-linter-errors

--- spec/my_list_spec.rb
+++ spec/my_list_spec.rb
@@ -1,15 +1,15 @@
-require_relative '../my_list.rb'
+require_relative '../my_list'
 
 describe 'Unit tests for MyList class' do
   it 'should return all by given logic' do
     list = MyList.new(1, 2, 3, 4)
-    expect(list.all? { |e| e < 5 }).to eq(true);
-    expect(list.all? { |e| e == 5 }).to eq(false); # as e > 5
+    expect(list.all? { |e| e < 5 }).to eq(true)
+    expect(list.all? { |e| e == 5 }).to eq(false) # as e > 5
   end
 
   it 'should return any by given logic' do
     list = MyList.new(1, 2, 3, 4)
-    expect(list.any? { |e| e == 2 }).to eq(true);
-    expect(list.any? { |e| e == 5 }).to eq(false);
+    expect(list.any? { |e| e == 2 }).to eq(true)
+    expect(list.any? { |e| e == 5 }).to eq(false)
   end
 end
